766daafc0df9a855a06752477a63fc385c92fbf8
Oliveiralsdo <oliveira.lsdo@gmail.com>
2022-04-20 09:03:35 -0300

    first commit

---
+++ src/app/nova-transferencia/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
